3ab58a4e155723351b7414c853f7e444ee378b01
Alphonso Wisseh <alphonso.wissehjr@gmail.com>
2019-04-26 17:53:13 -0400

    Get Api key and get data in JSON format

--- src/App.js
+++ src/App.js
@@ -2,10 +2,20 @@ import React, { Component } from 'react';
 import './App.css';
 import Form from './components/Form'
 
+const API_KEY = '731dd367ca87f8649a564f647a99554d';
+
 class App extends Component {
-  getRecipe = (e) => {
+  getRecipe = async (e) => {
+    const recipeName = e.target.elements.recipeName.value;
     e.preventDefault();
-    console.log('workingx')
+
+    //Make API Call *Prefix localhost with cors-anywhere link for cross origin error*
+    const api_call = await fetch(`https://cors-anywhere.herokuapp.com/https://www.food2fork.com/api/search?key=${API_KEY}&q=${recipeName}`);
+    
+    //Get API data in JSON format
+    const data = await api_call.json();
+
+    console.log(data)
   }
   render() {
     return (
