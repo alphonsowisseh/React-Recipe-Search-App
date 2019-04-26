833edb19cadbc160db60f44857c939e214a4796a
Alphonso Wisseh <alphonso.wissehjr@gmail.com>
2019-04-26 17:52:47 -0400

    Add name to form

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Form(props) {
   return (
     <form onSubmit={props.getRecipe}>
-        <input type="text"/>
+        <input type="text" name="recipeName"/>
         <button>Search</button>
     </form>
   )
