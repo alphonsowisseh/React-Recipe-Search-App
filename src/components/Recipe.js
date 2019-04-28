4a4ecea7926af56ae3687ae83026171ac4254e7c
Alphonso Wisseh <alphonso.wissehjr@gmail.com>
2019-04-28 19:24:05 -0400

    Update recipes.js
    add active recipe state and fetch active recipe

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-export default function Recipe() {
-  return (
-    <div>
-      Recipe Component!
-    </div>
-  )
+const API_KEY = '731dd367ca87f8649a564f647a99554d';
+
+export class Recipe extends Component {
+    state = {
+        activeRecipe: []
+    }
+
+    componentDidMount = async () => {
+        const title = this.props.location.state.recipe;
+        const req = await fetch(`https://cors-anywhere.herokuapp.com/https://www.food2fork.com/api/search?key=${API_KEY}&q=${title}`);
+
+        const res = await req.json();
+
+        this.setState({ activeRecipe: res.recipes[0] });
+
+    }
+  render() {
+    return (
+      <div>
+      </div>
+    )
+  }
 }
+
+export default Recipe
+
