8bd501e87afe2e86356d4e57ec8a286d367a9722
Alphonso Wisseh <alphonso.wissehjr@gmail.com>
2019-04-28 16:27:39 -0400

    Add some styles

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 export default function Form(props) {
   return (
-    <form onSubmit={props.getRecipe}>
-        <input type="text" name="recipeName"/>
-        <button>Search</button>
+    <form onSubmit={props.getRecipe} style={{ marginBottom: '2rem' }}>
+        <input type="text" name="recipeName" className="form__input"/>
+        <button className="form__button">Search</button>
     </form>
   )
 }
